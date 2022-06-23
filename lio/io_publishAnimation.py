a43ee68202e13ab102f20eea4d04a10c6171411d
CirkusBackup <juggler@cirkus.co.nz>
2022-06-23 12:23:31 +1200

    Patched anim exporter

--- lio/io_publishAnimation.py
+++ lio/io_publishAnimation.py
@@ -6,7 +6,7 @@ import os
 
 
 # export animation
-def publishFile(abcFilename):
+def publishFile(abcFilename, preroll: dict = {}):
     # get workspace
     workspace = cmds.workspace(q=True, directory=True, rd=True)
     workspaceLen = len(workspace.split('/'))
@@ -50,16 +50,21 @@ def publishFile(abcFilename):
                              'rsMaxDisplacement', 'rsDisplacementScale']
     for attr in additionalAttributes:
         additionalAttr += ' -attr %s' % (attr)
+
+    prerollStart = preroll.get('start', startFrame - 1)
+    prerollStep = preroll.get('step', 1)
+
     command = dedent(f'''
+    -frameRange {prerollStart} {startFrame - 1} -step {prerollStep} -preroll
     -frameRange {startFrame} {endFrame}{additionalAttr}
     -uvWrite
     -writeVisibility
-    -writeWholeFrameGeo
+    -wholeFrameGeo
     -worldSpace
     -writeUVSets
     -dataFormat ogawa{exportString}
     -file "{workspace}cache/alembic/{relativePath}{abcFilename}.abc"
-    ''')
+    ''').replace('\n', ' ')
     # load plugin
     if not cmds.pluginInfo('AbcExport', query=True, loaded=True):
         try:
@@ -69,7 +74,7 @@ def publishFile(abcFilename):
             cmds.error('Could not load AbcExport plugin')
     # write to disk
     cmds.AbcExport(j=command)
-    return '%scache/alembic/%s%s.abc' % (workspace, relativePath, abcFilename)
+    return f'{workspace}cache/alembic/{relativePath}{abcFilename}.abc'
 
 
 def runSilent():
@@ -85,7 +90,18 @@ def runWithUI():
     prefixString = cmds.textField('prefixText', q=True, text=True)
     nameString = cmds.textField('nameText', q=True, text=True)
     publishName = prefixString + '_' + nameString
-    publishFile(publishName)
+    prerollstart = cmds.intField('preroll_start', q=True, v=True)
+    prerollStep = cmds.floatField('preroll_step', q=True, v=True)
+    preroll = {}
+
+    if cmds.checkBox('preroll_enabled', q=True, v=True):
+        preroll = {
+            'start': prerollstart, 
+            'step': prerollStep
+        }
+
+    delete_anim_window()
+    publishFile(publishName, preroll)
 
 
 def createFilenames():
@@ -112,7 +128,7 @@ def createFilenames():
     return [filename, publishName]
 
 
-def anim_setText():
+def anim_setText(*args):
     # get filename
     names = createFilenames()
     cmds.textField('prefixText', e=True, tx=names[0])
@@ -122,43 +138,56 @@ def anim_setText():
 def IO_publishAnim_window():
     # UI objects
 
-    publishForm = cmds.formLayout()
-    prefixLabel = cmds.text(label='Prefix')
-    prefixText = cmds.textField('prefixText', w=250)
-    textLabel = cmds.text(label='Publish Name')
-    nameText = cmds.textField('nameText', w=250)
-    reloadButton = cmds.iconTextButton(style='iconOnly', image1='refresh.png', c='anim_setText()')
+    layout = cmds.rowColumnLayout(adj=1)
+    cmds.rowLayout(p=layout, nc=3, cw=[1, 100])
+    cmds.text(label='Prefix', align='right')
+    cmds.textField('prefixText', w=250)
+
+    cmds.rowLayout(p=layout, nc=3, cw=[1, 100])
+    cmds.text(label='Publish Name', align='right')
+    cmds.textField('nameText', w=250)
+    cmds.iconTextButton(style='iconOnly', image1='refresh.png', c=anim_setText)
+
+    cmds.rowColumnLayout(p=layout, adj=1)
+    cmds.text(l='', h=20)
+    cmds.text(ww=True,l='Preroll is used for simulation. Start defines what frame to start the preroll, step defines how large of a step each frame is. Preroll run before the alembic starts exporting.')
+
+    cmds.rowLayout(p=layout, nc=6, cw=[1, 100])
+    cmds.text(label='Preroll', align='right')
+    cmds.text(label='Start')
+    cmds.intField('preroll_start')
+    cmds.text(label='Step')
+    cmds.floatField('preroll_step', step=0.01, v=1)
+    cmds.checkBox('preroll_enabled', l='Enabled', v=0)
+
+    cmds.rowColumnLayout(p=layout, adj=1)
+    cmds.text(l='', h=20)
+    publishForm = cmds.formLayout(p=layout, h=50)
     btn1 = cmds.button(l='Publish', h=50, c='runWithUI()')
-    btn2 = cmds.button(l='Close', h=50, c='cmds.deleteUI(\'Publish Animation Window\')')
+    btn2 = cmds.button(l='Close', h=50, c='cmds.deleteUI("Publish Animation Window")')
     # UI layout
     cmds.formLayout(
         publishForm,
         edit=True,
         attachForm=[
-            (prefixLabel, 'top', 15),
-            (prefixLabel, 'left', 10),
-            (reloadButton, 'right', 10),
-            (prefixText, 'top', 10),
-            (textLabel, 'left', 10),
             (btn1, 'bottom', 0),
             (btn1, 'left', 0),
             (btn2, 'bottom', 0),
             (btn2, 'right', 0)
         ],
         attachControl=[
-            (prefixText, 'left', 10, textLabel),
-            (prefixText, 'right', 10, reloadButton),
-            (textLabel, 'top', 10, prefixLabel),
-            (nameText, 'top', 10, prefixLabel),
-            (nameText, 'left', 10, textLabel),
-            (nameText, 'right', 10, reloadButton),
-            (reloadButton, 'top', 10, prefixLabel),
             (btn2, 'left', 0, btn1)
         ],
         attachPosition=[
             (btn1, 'right', 0, 50)
         ])
     anim_setText()
+
+
+def delete_anim_window():
+    workspaceName = 'Publish Animation Window'
+    if (cmds.workspaceControl(workspaceName, exists=True)):
+        cmds.deleteUI(workspaceName)
 
 
 def IO_publishAnim(silent):
